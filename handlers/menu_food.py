b0fd5557d6abaf42d4798e295f970ef3c1dfbcc7
Mikhail Zashikhin <zmy007@mail.ru>
2024-08-09 15:07:57 +0300

    Recommendation fix

--- handlers/menu_food.py
+++ handlers/menu_food.py
@@ -715,15 +715,11 @@ def generate_recommendation(user):
         'Настроение',
         'Ссылка'
     ])
-
+    print(mood, style, restaurant, blacklist)
     # Выделяем только то меню, что сейчас запрашивает клиент
-    print(df.head() + "\n\n")
-    df = df[df['Название ресторана'].str.contains(restaurant)]
-    print(df.head() + "\n\n")
+    # df = df[df['Название ресторана'].str.contains(restaurant)]
     df = df[df['Настроение'].str.contains(mood)]
-    print(df.head() + "\n\n")
     df = df[df['Стиль питания'].str.contains(style)]
-    print(df.head() + "\n\n")
 
     if df.empty:
         return None
