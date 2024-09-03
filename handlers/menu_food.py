34fa522c4d5898db6bd57466a66df796d475e179
DarkCardinal <zmy007@mail.ru>
2024-09-03 23:20:32 +0300

    Filter update

--- handlers/menu_food.py
+++ handlers/menu_food.py
@@ -398,6 +398,8 @@ async def filters(call: types.CallbackQuery):
     user = call.from_user.id
     data = call.data.split('_')
     actual_filter = eval(db.get_client_filter(user))
+    if not actual_filter or actual_filter == "None":
+        actual_filter = {}
     if "cost" in data:
         actual_filter["cost"] = data[-1]
     if "cuisine" in data:
