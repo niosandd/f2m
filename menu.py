bf6ee4fd36448039fc9544e22da184c26a351079
DarkCardinal <zmy007@mail.ru>
2024-09-17 17:16:51 +0300

    DB update

--- menu.py
+++ menu.py
@@ -241,9 +241,6 @@ def read_table_2(user, mood: str, style: str, blacklist: str):
     for dish in df.values.tolist():
         dish_ingredients = [ingredient.strip() for ingredient in str(dish[19]).lower().split(',')]
         if set(blacklist) & set(dish_ingredients):
-            print("Пропускаем блюдо из-за запрещенного ингредиента:", dish[4])
-            print("Запрещенные ингредиенты:", blacklist)
-            print("Ингредиенты блюда:", dish_ingredients)
             continue
 
         dishes.append({
